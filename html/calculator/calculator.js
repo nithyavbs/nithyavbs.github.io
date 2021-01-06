9b3034fd68c463579826e93c0e3b11469f236a06
nithyavbs <nithiblossom@gmail.com>
2021-01-06 20:25:56 +0530

    Memory add and Mem clr function added

--- html/calculator/calculator.js
+++ html/calculator/calculator.js
@@ -1,20 +1,39 @@
 
-         //function that display value 
-         function dis(val) 
-         { 
-             document.getElementById("result").value+=val 
-         } 
-           
-         //function that evaluates the digit and return result 
-         function solve() 
-         { 
-             var x = document.getElementById("result").value 
-             var y = eval(x) 
-             document.getElementById("result").value = y 
-         } 
-           
-         //function that clear the display 
-         function clr() 
-         { 
-             document.getElementById("result").value = "" 
-         } 
+// Will hold values we want to store in memory
+let valMemStored = "";
+// Stores value we are building
+let newVal = "";
+//function that display value 
+function dis(val) {
+    document.getElementById("result").value += val
+}
+
+//function that evaluates the digit and return result 
+function solve() {
+    var x = document.getElementById("result").value
+    var y = eval(x)
+    document.getElementById("result").value = y
+}
+
+//function that clear the display 
+function clr() {
+    document.getElementById("result").value = ""
+}
+// Store the current value in the entry window
+function copyButPress() {
+    valMemStored = document.getElementById("result").value;
+}
+
+// If a value has been stored paste it in the entry
+// window and assign it as the newVal
+function pasteButPress() {
+    if (valMemStored) {
+        document.getElementById("result").value = valMemStored;
+        newVal = valMemStored;
+    }
+
+}
+function memClr() {
+    valMemStored = "";
+    document.getElementById("result").value = valMemStored;
+}
