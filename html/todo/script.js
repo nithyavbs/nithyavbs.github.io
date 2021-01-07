813e1dae0dae8df39d5d6170824d110a2733f824
nithyavbs <nithiblossom@gmail.com>
2021-01-07 21:55:07 +0530

    headings size changed

--- html/todo/script.js
+++ html/todo/script.js
@@ -1,22 +1,22 @@
 
 function addItem() {
-    var li = document.createElement("li"); 
+    var li = document.createElement("li");
     var ul = document.querySelector("ul");
     var new_item = document.getElementById("newItem").value
-    if (new_item == '') {
-      alert("You must add item!");
-         } else {
-    // creates an element "li"
-	li.appendChild(document.createTextNode(new_item));
-    ul.appendChild(li); //adds li to ul
-    document.getElementById("newItem").value=""; //Reset text input field
-   	var del_button = document.createElement("button");
-	del_button.appendChild(document.createTextNode("X"));
-    li.appendChild(del_button);
-    del_button.onclick = function (){ 
-     var div = this.parentElement;
-     div.parentNode.removeChild(div);
+    if (new_item == "") {
+        alert("You must add item!");
+    }
+    else {
+        // creates an element "li"
+        li.appendChild(document.createTextNode(new_item));
+        ul.appendChild(li); //adds li to ul
+        document.getElementById("newItem").value = ""; //Reset text input field
+        var del_button = document.createElement("button");
+        del_button.appendChild(document.createTextNode("X"));
+        li.appendChild(del_button);
+        del_button.onclick = function () {
+            var div = this.parentElement;
+            div.parentNode.removeChild(div);
+        }
     }
 }
-}
-
